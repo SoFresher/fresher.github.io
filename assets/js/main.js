62719d1b569c94f1fb1dbdf7301aaead91e9d007
Uthman <Soliu Usman Fresh'er>
2019-08-31 12:59:16 +0100

    single project hover implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(function () {
+	// Initializing easeScroll for smoth scrolling
   	$("html").easeScroll();
 
+	// navbar scroll
 	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
 		    $(".navbar-fixed-top").addClass("scrolled");
@@ -8,6 +10,8 @@ $(function () {
 		    $(".navbar-fixed-top").removeClass("scrolled");
 		}
 	});
+
+	// toggle scrolled if scrolled before reload
 	$(".navbar").ready(function() {
 		if ($(".navbar").offset().top > 50) {
 		    $(".navbar-fixed-top").addClass("scrolled");
@@ -16,6 +20,7 @@ $(function () {
 		}
 	});
 
+	// Mouse over single featured service
 	$(".featured-single-service").hover(
 		function () {
 			$(this).find(".icon-shape").css("border-radius", "50px");
@@ -24,53 +29,72 @@ $(function () {
 			$(this).find(".icon-shape").css("border-radius", ".25rem");
 		}
 	);
-
-	$(function() {
-        $(document).on('click', 'a.page-scroll', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top-59
-            }, 900, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
-	$(function(){
-		$('body').append('<div id="toTop"><span><i class="fa fa-angle-down"></i></span></div>');
-
-		$(window).on("scroll", function (e) {
-		    if ($(this).scrollTop() != 0) {
-		        $('#toTop').fadeIn();
-		    } else {
-		        $('#toTop').fadeOut();
-		    }
-		});
-
-		$('#toTop').on('click',function(){
-		    $("html, body").animate({ scrollTop: 0 }, 600);
-		    return false;
-		});
-
+	
+	// Page scroll navigation
+	$(document).on('click', 'a.page-scroll', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top-59
+		}, 900, 'easeInOutExpo');
+		event.preventDefault();
 	});
+
+	// Scroll to Top nav
+	$('body').append('<div id="toTop"><span><i class="fa fa-angle-down"></i></span></div>');
+
+	$(window).on("scroll", function (e) {
+		if ($(this).scrollTop() != 0) {
+			$('#toTop').fadeIn();
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
+
+	$('#toTop').on('click',function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+
+	// Preloader
 	$(window).ready(function() {
         $('#preloader').delay(500).fadeOut('fade');
-    });
+	});
+	
+	// Scroll to set active nav item
     $(window).scroll(function(){
     	var scrollbarLocation=$(this).scrollTop();
     	$(".nav-link").each(function(){
-    		console.log($(this.hash).offset().top);
-    		var sectionOffset=$(this.hash).offset().top-73;
+    		var sectionOffset=$(this).offset().top-73;
     		if(sectionOffset <= scrollbarLocation){
     			$(this).parent().addClass('active');
     			$(this).parent().siblings().removeClass('active');
     		}
     	});
-    })
-    if($('.progress-line').length){
-        $('.progress-line').appear(function(){
-            var el = $(this);
-            var percent = el.data('width');
-            $(el).css('width',percent+'%');
-        },{accY: 0});
-    }
+	})
+	
+	// Progress line skills
+    // if($('.progress-line').length){
+    //     $('.progress-line').appear(function(){
+    //         var el = $(this);
+    //         var percent = el.data('width');
+    //         $(el).css('width',percent+'%');
+    //     },{accY: 0});
+	// }
+
+	// Overlay on hover
+	// $('.single-project').mouseover(function () {
+	// 	$(this).find('#overlay').css('display', 'flex');
+	// 	console.log('yes');
+	// }).mouseleave(function () {
+	// 	$(this).find('#overlay').css('display', 'none');
+	// });
+	$(".single-project").hover(
+		function () {
+			$(this).find("#overlay").css("display", "flex");
+		},
+		function () {
+			$(this).find("#overlay").css("display", "none");
+		}
+	);
 });
 
